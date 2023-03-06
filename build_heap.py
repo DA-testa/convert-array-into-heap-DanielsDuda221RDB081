e54a1434f80523685bcdc57502b912cfa41b7f01
Dāniels Dūda 221RDB081 <124591745+DanielsDuda221RDB081@users.noreply.github.com>
2023-03-06 23:18:08 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,10 +1,46 @@
 # python3
+from array import array
 
+def build_heap(a):
+    n = len(a)
+    swaps = [[0,0]]
+    swaps.pop([0][0])
+    k=-1
+    i=0
+    check = False
 
-def build_heap(data):
-    swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    while(True):
+        if check==True and k==1:
+            break
+        if i == (n-1)/2:
+            check = True
+        if check ==True:
+            k=k-2
+        else:
+            k=k+2
+        
+        if a[n-k]>a[n-(k+1)]:
+            if (k+3)>n:
+                if a[n-(k+1)]<a[n-(k+2)]:
+                    swaps.append([n-(k+2), n-(k+1)])
+                    a[n-(k+1)], a[n-(k+2)] = a[n-(k+2)], a[n-(k+1)] 
+            else:  
+                if a[n-(k+1)]<a[n-(k+3)]:
+                    swaps.append([n-(k+3), n-(k+1)])
+                    a[n-(k+1)], a[n-(k+3)] = a[n-(k+3)], a[n-(k+1)]
+
+        else:
+            if (k+3)>n:
+                if a[n-k]<a[n-(k+2)]:
+                    swaps.append([n-(k+2), n-k])
+                    a[n-k], a[n-(k+2)] = a[n-(k+2)], a[n-k] 
+            else:  
+                if a[n-k]<a[n-(k+3)]:
+                    swaps.append([n-(k+3), n-k])
+                    a[n-k], a[n-(k+3)] = a[n-(k+3)], a[n-k]
+
+        i=i+1
+    
     
 
 
@@ -13,14 +49,24 @@ def build_heap(data):
 
 def main():
     
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+    text = input()
+    if "I" in text:
+        n = int(input())
+        data = input()
+    elif "F" in text:
+        text2 = input()
+        if "a" in text2:
+            return()
+        with open ("test/"+text2, encoding="utf-8") as fails:
+            n = int (fails.readline())
+            data = fails.readline()
+            
+    else:
+        return()
 
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    
+    data = list(map(int, data.split()))
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
